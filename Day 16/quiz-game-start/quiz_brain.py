47c10ac6c401f7c110f00e09293657d560e82c52
ruskram <ruskram87@gmail.com>
2026-01-14 18:10:53 -0500

    new Updates

--- Day 16/quiz-game-start/quiz_brain.py
+++ Day 16/quiz-game-start/quiz_brain.py
@@ -9,17 +9,19 @@ class QuizBrain:
     def next_question(self):
         question = self.question_list[self.question_number]
         self.question_number += 1
-        user_answer = input(f'Q.{self.question_number}: {question.text} (True/False)?')
+        user_answer = input(f'Q.{self.question_number}: {question.text} (True/False)? ')
         self.check_answer(user_answer, question.answer)
 
 
     def check_answer(self, user_answer, correct_answer):
         if user_answer.lower() == correct_answer.lower():
             print("You got it right!")
-            self.score += 1
-            print(f"Your current score is: {self.score}/{self.question_number}")
+            self.score += 1            
         else:
             print("That's wrong.")
+        print(f"Your current score is: {self.score}/{self.question_number}")
+        print(f"The correct answer was: {correct_answer}.\n")
+
 
     def start_quiz(self):
         while self.question_number < len(self.question_list):
